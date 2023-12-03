7b80c9f09298e01aa4951febc4498b8224df1c6f
Rezardy <61234961+Rezardy@users.noreply.github.com>
2023-12-03 21:56:56 +0700

    Clean Day-1.py
    Removed that weird log of combination words I used as reference

--- Day-1.py
+++ Day-1.py
@@ -7,16 +7,7 @@ start = question.split("\r\n")
 
 Numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 Digits = ["zer0ero", "on1ne", "tw2wo", "thre3hree", "fou4our", "fiv5ive", "si6ix", "seve7even", "eigh8ight", "nin9ine"]
-#each number possibility only shares at most 1 character
-#zer[o]ne
-#on[e]ight
-#eigh[t]wo
-#tw[o]ne
-#eigh[t]hree
-#thre[e]ight
-#fiv[e]ight
-#seve[n]ine
-#nin[e]ight
+#replaces number words with the digit and part of the word to combat potential long 3-8-3-8 chains
 
 def checkNum(fString):
     for findex in range(10):
@@ -26,9 +17,8 @@ def checkNum(fString):
 def readDigit(fString, Dir):
     #Input string, then 1 if forward, -1 if backwards
     findex = int(-0.5 + Dir/2)
-    fString = checkNum(fString)
+    fString = checkNum(fString) #check the whole string for number-words
     fchar = fString[findex]
-    #print(fString)
     while fchar.isdigit() == False:
         findex += Dir #Look at the next character
         fchar = fString[findex]
@@ -38,7 +28,5 @@ result = 0
 for cal in start:
     first = readDigit(cal, 1)
     last = readDigit(cal, -1)
-    #print(cal)
-    #print(first, last)
     result += int(first + last)
 print(result)
