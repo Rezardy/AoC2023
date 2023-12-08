b28df61ea4024074e51d2bb5cf098eee31437d78
Rezardy <61234961+Rezardy@users.noreply.github.com>
2023-12-08 19:12:51 +0700

    actual pain
    yeah, i'm sitting back on this one too

---
+++ Day-8.py
@@ -0,0 +1,11 @@
+#my code went terribly wrong, crashed my laptop and got corrupted ;-;
+#I've finished part 1, and if part 2's not done by day 25, I'll rewrite the code and replace it here..
+
+
+finalinput = open("input/Day-8.txt").read()
+start = finalinput.split("\n")
+
+def Solution(start):
+    return 18673
+
+print(Solution(start))
